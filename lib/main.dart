db805dd94aa9b4131db98f31fc967cf7ccfccf31
kerlos <kerlosaitf16@gmail.com>
2025-01-21 01:29:30 +0200

    change thame

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:note/homePage.dart';
+import 'package:note/views/notes_view.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,7 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
       home: Scaffold(
         body: Homepage(),
       ),
