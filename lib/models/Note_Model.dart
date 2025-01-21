6ba3601af101e528fcbde9da232391ec730a89e5
kerlos <kerlosaitf16@gmail.com>
2025-01-21 19:27:52 +0200

    add hive

--- lib/models/Note_Model.dart
+++ lib/models/Note_Model.dart
@@ -1,8 +1,18 @@
+import 'package:hive/hive.dart';
+
+part 'Note_Model.g.dart';
+
+@HiveType(typeId: 0)
 class NoteModel {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subtitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
+
   NoteModel({
     required this.date,
     required this.subtitle,
