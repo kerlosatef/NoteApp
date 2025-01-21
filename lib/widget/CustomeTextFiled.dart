db41fb3d05b8eeb98d7aa3bcaaebe0698b7f8ea7
kerlos <kerlosaitf16@gmail.com>
2025-01-21 21:42:16 +0200

    vallidtor

--- lib/widget/CustomeTextFiled.dart
+++ lib/widget/CustomeTextFiled.dart
@@ -2,14 +2,25 @@ import 'package:flutter/material.dart';
 
 class CustomeTextField extends StatelessWidget {
   const CustomeTextField(
-      {super.key, this.expandeBool = false, required this.texthint});
+      {super.key,
+      this.expandeBool = false,
+      required this.texthint,
+      this.onSaved});
 
   final String texthint;
   final bool expandeBool;
-
+  final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      validator: (value) {
+        if (value?.isEmpty ?? true) {
+          return "Field is Required";
+        } else {
+          return null;
+        }
+      },
+      onSaved: onSaved,
       maxLines: null,
       expands: expandeBool,
       decoration: InputDecoration(
--- lib/widget/Notes_sheet.dart
+++ lib/widget/Notes_sheet.dart
@@ -3,43 +3,82 @@ import 'package:note/widget/CustomeTextFiled.dart';
 
 class NotesSheet extends StatelessWidget {
   const NotesSheet({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: 20),
-        Padding(
-          padding: EdgeInsets.all(8.0),
-          child: CustomeTextField(
-            texthint: "Title",
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SizedBox(
-            height: 160,
-            child: CustomeTextField(texthint: "Content", expandeBool: true),
-          ),
-        ),
-        SizedBox(height: 4),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: InkWell(
-            onTap: () {},
-            child: Container(
-              alignment: AlignmentDirectional.center,
-              child: Text(
-                "ADD",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-              height: 50,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20), color: Colors.blue),
+    return AddNoteForm();
+  }
+}
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formkey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  String? title, subtitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formkey,
+      autovalidateMode: AutovalidateMode.disabled,
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: CustomeTextField(
+              onSaved: (value) {
+                title = value;
+              },
+              texthint: "Title",
             ),
           ),
-        )
-      ],
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: 160,
+              child: CustomeTextField(
+                  onSaved: (value) {
+                    subtitle = value;
+                  },
+                  texthint: "Content",
+                  expandeBool: true),
+            ),
+          ),
+          SizedBox(height: 4),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: () {
+                if (formkey.currentState!.validate()) {
+                  formkey.currentState!.save();
+                } else {
+                  autovalidateMode = AutovalidateMode.always;
+                  setState(() {});
+                }
+              },
+              child: Container(
+                alignment: AlignmentDirectional.center,
+                child: Text(
+                  "ADD",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blue),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
