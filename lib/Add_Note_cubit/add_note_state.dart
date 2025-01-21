4dfcaead0a9a9cd913d9886de6db820c27a68380
kerlos <kerlosaitf16@gmail.com>
2025-01-21 22:12:16 +0200

    add mode stats

--- lib/Add_Note_cubit/add_note_state.dart
+++ lib/Add_Note_cubit/add_note_state.dart
@@ -4,3 +4,12 @@ part of 'add_note_cubit.dart';
 abstract class AddNoteState {}
 
 class AddNoteInitial extends AddNoteState {}
+
+class AddNoteSuccess extends AddNoteState {}
+
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteFailuer extends AddNoteState {
+  final String errMessage;
+  AddNoteFailuer(this.errMessage);
+}
