ceab8ec061a563bbdd93637f39c51a7c2975909e
Kerlos <kerloskoko992@yahoo.com>
2025-01-22 15:31:15 +0200

    add note cubie to add note bottom

--- lib/widget/Notes_sheet.dart
+++ lib/widget/Notes_sheet.dart
@@ -1,84 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:note/Add_Note_cubit/add_note_cubit.dart';
 import 'package:note/widget/CustomeTextFiled.dart';
+import 'package:note/widget/add_note_form.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
-class NotesSheet extends StatelessWidget {
-  const NotesSheet({super.key});
+class NotesSheet extends StatefulWidget {
+  NotesSheet({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return AddNoteForm();
-  }
+  State<NotesSheet> createState() => _NotesSheetState();
 }
 
-class AddNoteForm extends StatefulWidget {
-  const AddNoteForm({
-    super.key,
-  });
+class _NotesSheetState extends State<NotesSheet> {
+  bool isLoading = false;
 
-  @override
-  State<AddNoteForm> createState() => _AddNoteFormState();
-}
-
-class _AddNoteFormState extends State<AddNoteForm> {
-  final GlobalKey<FormState> formkey = GlobalKey();
-  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
-
-  String? title, subtitle;
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: formkey,
-      autovalidateMode: autovalidateMode,
-      child: Column(
-        children: [
-          SizedBox(height: 20),
-          Padding(
-            padding: EdgeInsets.all(8.0),
-            child: CustomeTextField(
-              onSaved: (value) {
-                title = value;
-              },
-              texthint: "Title",
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              height: 160,
-              child: CustomeTextField(
-                  onSaved: (value) {
-                    subtitle = value;
-                  },
-                  texthint: "Content",
-                  expandeBool: true),
-            ),
-          ),
-          SizedBox(height: 4),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: GestureDetector(
-              onTap: () {
-                if (formkey.currentState!.validate()) {
-                  formkey.currentState!.save();
-                } else {
-                  autovalidateMode = AutovalidateMode.always;
-                  setState(() {});
-                }
-              },
-              child: Container(
-                alignment: AlignmentDirectional.center,
-                child: Text(
-                  "ADD",
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                ),
-                height: 50,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.blue),
-              ),
-            ),
-          )
-        ],
-      ),
+    return BlocConsumer<AddNoteCubit, AddNoteState>(
+      listener: (context, state) {
+        if (state is AddNoteFailuer) {
+          print("failed ${state.errMessage}");
+        }
+        if (state is AddNoteSuccess) {
+          Navigator.pop(context);
+        }
+      },
+      builder: (context, state) {
+        return ModalProgressHUD(
+            inAsyncCall: state is AddNoteLoading ? true : false,
+            child: AddNoteForm());
+      },
     );
   }
 }
---
+++ lib/widget/add_note_form.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:note/widget/CustomeTextFiled.dart';
+
+class AddNoteForm extends StatefulWidget {
+  const AddNoteForm({
+    super.key,
+  });
+
+  @override
+  State<AddNoteForm> createState() => _AddNoteFormState();
+}
+
+class _AddNoteFormState extends State<AddNoteForm> {
+  final GlobalKey<FormState> formkey = GlobalKey();
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  String? title, subtitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formkey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: CustomeTextField(
+              onSaved: (value) {
+                title = value;
+              },
+              texthint: "Title",
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: SizedBox(
+              height: 160,
+              child: CustomeTextField(
+                  onSaved: (value) {
+                    subtitle = value;
+                  },
+                  texthint: "Content",
+                  expandeBool: true),
+            ),
+          ),
+          SizedBox(height: 4),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: GestureDetector(
+              onTap: () {
+                if (formkey.currentState!.validate()) {
+                  formkey.currentState!.save();
+                } else {
+                  autovalidateMode = AutovalidateMode.always;
+                  setState(() {});
+                }
+              },
+              child: Container(
+                alignment: AlignmentDirectional.center,
+                child: Text(
+                  "ADD",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+                height: 50,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.blue),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
